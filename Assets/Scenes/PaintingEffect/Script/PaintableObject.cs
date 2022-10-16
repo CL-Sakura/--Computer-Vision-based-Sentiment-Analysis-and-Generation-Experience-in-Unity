31c87b588833f7b69d02a4f2c023d307063aa6af
LiKira <422799872@qq.com>
2022-10-16 18:17:21 +0800

    Update painting system

---
+++ Assets/Scenes/PaintingEffect/Script/PaintableObject.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintableObject : MonoBehaviour
+{
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Shader drawShader;
+
+
+    private RenderTexture splatMap;
+    private Material currentMaterial,drawMaterial;
+    private RaycastHit hit;
+
+    [SerializeField]
+    [Range(1, 500)] private float size;
+
+    [SerializeField]
+    [Range(0, 1)] private float strength;
+
+
+    void Start()
+    {
+        drawMaterial = new Material(drawShader);
+        drawMaterial.SetVector("_Color", Color.red);
+
+        currentMaterial = GetComponent<MeshRenderer>().material;
+
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        currentMaterial.SetTexture("SplatMap", splatMap);
+
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition),out hit))
+            {
+                drawMaterial.SetVector("_Coordinates", new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0, 0));
+                drawMaterial.SetFloat("_Strength", strength);
+                drawMaterial.SetFloat("_Size", size);
+                RenderTexture temp = RenderTexture.GetTemporary(splatMap.width,splatMap.height,0, RenderTextureFormat.ARGBHalf);
+                Graphics.Blit(splatMap, temp);
+                Graphics.Blit(temp,splatMap, drawMaterial);
+                RenderTexture.ReleaseTemporary(temp);
+            }
+
+
+        }
+    }
+}
